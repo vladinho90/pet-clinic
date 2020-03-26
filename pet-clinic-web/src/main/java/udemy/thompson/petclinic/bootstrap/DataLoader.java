c1138ac66034715ddd211e5387326d29daf63461
Vladinho1990 <vldburlacu@yahoo.com>
2020-03-27 00:43:38 +0200

    added PetTypes to bootstrap  .Closes #31

--- pet-clinic-web/src/main/java/udemy/thompson/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/udemy/thompson/petclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package udemy.thompson.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import udemy.thompson.petclinic.model.Owner;
+import udemy.thompson.petclinic.model.PetType;
 import udemy.thompson.petclinic.model.Vet;
 import udemy.thompson.petclinic.services.OwnerService;
+import udemy.thompson.petclinic.services.PetTypeService;
 import udemy.thompson.petclinic.services.VetService;
 
 @Component
@@ -12,14 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -35,13 +49,13 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Loaded Owners....");
 
-        Vet vet1=new Vet();
+        Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
-        Vet vet2=new Vet();
+        Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
