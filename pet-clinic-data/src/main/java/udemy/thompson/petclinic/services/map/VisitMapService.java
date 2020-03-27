54754ca3d59b0d13794ac14465b16df8171903d9
Vladinho1990 <vldburlacu@yahoo.com>
2020-03-27 18:04:33 +0200

    implemented JPA Visit Service, added loader data .Closes #46

--- pet-clinic-data/src/main/java/udemy/thompson/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/udemy/thompson/petclinic/services/map/VisitMapService.java
@@ -1,10 +1,11 @@
 package udemy.thompson.petclinic.services.map;
 
+import org.springframework.stereotype.Service;
 import udemy.thompson.petclinic.model.Visit;
 import udemy.thompson.petclinic.services.VisitService;
 
 import java.util.Set;
-
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
 
---
+++ pet-clinic-data/src/main/java/udemy/thompson/petclinic/services/springdatajpa/VisitSDJPAService.java
@@ -0,0 +1,48 @@
+package udemy.thompson.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import udemy.thompson.petclinic.model.Visit;
+import udemy.thompson.petclinic.repositories.VisitRepository;
+import udemy.thompson.petclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
