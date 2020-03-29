dc05991d4d07567023668bea85e3ef107958d306
Vladinho1990 <vldburlacu@yahoo.com>
2020-03-29 11:57:24 +0300

    added test for empty find owner last name by to return all. Closes #64

--- pet-clinic-web/src/test/java/udemy/thompson/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/udemy/thompson/petclinic/controllers/OwnerControllerTest.java
@@ -80,6 +80,20 @@ class OwnerControllerTest {
     }
 
     @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                        Owner.builder().id(2L).build()));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+
+    }
+
+    @Test
     void displayOwner() throws Exception {
 
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
@@ -91,7 +105,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    void initCreationForm() throws Exception{
+    void initCreationForm() throws Exception {
         mockMvc.perform(get("/owners/new"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"))
@@ -100,7 +114,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    void processCreationForm() throws Exception{
+    void processCreationForm() throws Exception {
         when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
 
         mockMvc.perform(post("/owners/new"))
@@ -113,7 +127,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    void initUpdateOwnerForm() throws Exception{
+    void initUpdateOwnerForm() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
 
         mockMvc.perform(get("/owners/1/edit"))
@@ -125,7 +139,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    void processUpdateOwnerFForm() throws Exception{
+    void processUpdateOwnerFForm() throws Exception {
         when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
 
         mockMvc.perform(post("/owners/1/edit"))
